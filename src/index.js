d434f9babf7fe7abf0f776370171ab0d00799a8c
Адамов Александр Михайлович <aadamov@cs.local>
2018-01-11 17:12:44 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,21 +3,49 @@ TODO
 
 */
 
-const { Bot } = require("@dlghq/dialog-bot-sdk");
-const path = require("path");
-const config = require("./config");
-const API = require("./API");
+const { Bot } = require('@dlghq/dialog-bot-sdk');
+// const path = require('path');
+const config = require('./config');
+const API = require('./API');
 
 const bot = new Bot({
-  endpoints: ["wss://ws1.dlg.im"],
-  phone: "7555123321",
-  code: "5555"
+  endpoints: ['wss://ws1.coopintl.com', 'wss://ws2.coopintl.com'],
+  phone: '755512345',
+  code: '5555',
 });
 
-const state = {};
+// const state = {};
 
-const users = {};
+bot.onMessage(async (peer, message) => {
+  API.DefineNewUser(peer);
+  if (config.users[peer.id].startedButton && peer.type !== 'group' && API.IfPeerAdmin(peer)) {
+    API.greetings(bot, peer);
+  }
+  if (config.users[peer.id].verification) API.AskQuestions(bot, peer, message);
+  if (config.users[peer.id].edit) API.EditInfo(bot, peer, message);
+  if (
+    config.users[peer.id].verified === true &&
+    (message.content.text.toLowerCase() === 'исправить' ||
+      message.content.text.toLowerCase() === 'bcghfdbnm' ||
+      message.content.text.toLowerCase() === 'и')
+  ) {
+    API.checking(bot, peer);
+  }
+});
 
-bot.onMessage(async (peer, message) => {});
-
-bot.onInteractiveEvent(async event => {});
+bot.onInteractiveEvent(async (event) => {
+  if (event.value === 'start') {
+    API.AskQuestions(bot, event.peer);
+    config.users[String(event.peer.id)].verification = true;
+  }
+  if (
+    event.value === 'fio' ||
+    event.value === 'birth' ||
+    event.value === 'region' ||
+    event.value === 'vacation' ||
+    event.value === 'status' ||
+    event.value === 'spehre'
+  ) {
+    API.SelectEditInfo(event, bot);
+  }
+});
